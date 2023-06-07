54e5dfdb6a911d6c44821ababc99d3b7423a4c86
Ayame Shirai <ayameshirai@ReasenoMacBook-Pro.local>
2023-06-07 23:12:08 +0900

    index_update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,18 @@ import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useAuth, useUser } from "../hooks/firebase";
 import Signup from "./signup";
+import auth from "firebase/auth";
 
 function Home() {
   const router = useRouter();
-  const currentUser = useUser();
+  const auth = useAuth(); // useAuthフックを使って認証情報を取得
 
-  // ログイン済みのユーザーがいる場合は別の画面にリダイレクトする
   useEffect(() => {
-    if (currentUser) {
-    }
-  }, [currentUser]);
+    // ログイン済みのユーザーがいる場合は別の画面にリダイレクトする
+    const currentUser = auth.currentUser;
+  }, [auth, router]);
 
   return <Signup />;
 }
 
-export default Home
+export default Home;
